99ea50a42cf78588dc1054f7b9c743da85f851aa
Filipa Andrade <filipa.andrade@gmail.com>
2019-07-24 19:56:01 +0200

    Add working example of charts with seaborn on linux

---
+++ meetup07/dv_seaborn.py
@@ -0,0 +1,30 @@
+#from IPython import get_ipython
+#get_ipython().run_line_magic('matplotlib', 'qt5')
+
+import matplotlib
+matplotlib.use("Qt5Agg")
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+# Suppress warnings
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# Optional but changes the figure size
+fig = plt.figure(figsize=(12, 8))
+
+#df = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv')
+#df = pd.read_csv('/home/filipa/Downloads/mtcars.csv')
+df = pd.read_csv('/home/filipa/Downloads/Popular_Baby_Names.csv')
+
+# Line
+#ax = sns.regplot(x="Child's First Name", y="Count", data=df)
+
+# Histogram
+sns.distplot(df.Count, kde=False)
+
+# Show the plot
+plt.show()
