477aeaaca201beb3b5e26a5a66631e0bdd7cf240
Filipa Andrade <filipa.andrade@gmail.com>
2019-11-10 19:08:17 +0100

    Add 3 exercises from the live coding meetup

---
+++ coding01/exercise02.py
@@ -0,0 +1,10 @@
+# Write a Python program to display the current date and time.
+# Sample Output :
+# Current date and time :
+# 2014-07-05 14:34:14
+from datetime import datetime
+from dateutil import tz
+
+
+timezone = tz.gettz('Australia/Sydney')
+print(datetime.now(tz=timezone).strftime("%d.%m.%Y %H:%M:%S.%f %Z"))
