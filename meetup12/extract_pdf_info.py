cabfeaba6709b46e2fa51ba41ce4f1370eb09ecb
Filipa Andrade <filipa.andrade@gmail.com>
2019-12-07 15:35:21 +0100

    Add more info about tika

--- meetup12/extract_pdf_info.py
+++ meetup12/extract_pdf_info.py
@@ -20,6 +20,8 @@ for pdf_filename in os.listdir(pdf_dir):
 # For this to work you need java and the tike-server.jar file
 # Download it from https://www.apache.org/dyn/closer.cgi/tika/tika-server-1.23.jar
 # run it with java -jar tika-server.jar
+# you also need this env in your environment TIKA_SERVER_JAR="file:////tika-server.jar"
+# you can do it with `export TIKA_SERVER_JAR="file:////tika-server.jar"`
 for pdf_filename in os.listdir(pdf_dir):
     print("*** Parsing {} ***".format(pdf_filename))
     raw_text = parser.from_file(os.path.join(pdf_dir, pdf_filename))
