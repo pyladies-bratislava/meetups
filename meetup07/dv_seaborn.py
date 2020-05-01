5fa9ee6d1e49e8cd326b2f84d3e4dbc2cf51b04a
Filipa Andrade <filipa.andrade@gmail.com>
2020-05-01 11:20:58 +0200

    Add plot with the top 50 rows of data
    Use with names_large.csv

--- meetup07/dv_seaborn.py
+++ meetup07/dv_seaborn.py
@@ -13,6 +13,8 @@ warnings.filterwarnings('ignore')
 # Optional but changes the figure size
 fig = plt.figure(figsize=(12, 8))
 
+# There are 2 csv files with data in this folder
+# names_large.csv contains a large amount of data while names.csv is small
 df = pd.read_csv('names.csv')
 
 # Filtered as example but not used
@@ -24,6 +26,15 @@ filtered = df["Count"] == 5
 
 # Barplot
 sns.barplot(x="Child's First Name", y="Count", data=df)
+
+# Create df top 50
+df_top_50 = df.groupby("Child's First Name")["Count"].sum().sort_values(ascending=False).reset_index()
+df_top_50 = df_top_50.head(50)
+
+# Plot
+plt.figure(figsize=(20, 10))
+sns.barplot(x="Child's First Name", y="Count", data=df_top_50)
+plt.xticks(rotation=90)
 
 # Show the plot
 plt.show()
